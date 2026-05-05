f1dd1669b4805ab29bc5981ad4eda2bff4a1b0a7
shamyanarman121-rgb <shamyanarman121@gmail.com>
2026-05-05 17:30:15 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 // Increase payload limit to handle base64 encoded images
-app.use(express.json({ limit: '2mb' }));
-app.use(express.urlencoded({ extended: true, limit: '2mb' }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // Serve static files (like CSS if you had any)
 app.use(express.static(path.join(__dirname, '../')));
