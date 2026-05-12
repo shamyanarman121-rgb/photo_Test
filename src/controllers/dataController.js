2097101857adbe1b42b860590d7cd878f8d99285
shamyanarman121-rgb <shamyanarman121@gmail.com>
2026-05-12 11:14:04 +0400

    Update dataController.js

--- src/controllers/dataController.js
+++ src/controllers/dataController.js
@@ -38,7 +38,7 @@ const processData = async (req, res) => {
     });
     
     // Replace with the actual external API URL
-    const externalApiUrl = "https://totogaming.app.n8n.cloud/webhook/8bd18292-f8d4-46a0-80e9-e463ef8f457a" 
+    const externalApiUrl = "https://testanenq.app.n8n.cloud/webhook/1bfd1e55-0da4-4678-834a-133060e46c62" 
 
     const response = await axios.post(externalApiUrl, dataToSend, {
       timeout: 30000
